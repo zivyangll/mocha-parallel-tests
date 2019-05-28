3a3cb5be253c118c9ac344b431c730405802decd
zivyangll <zivyangll@gmail.com>
2019-05-28 18:06:03 +0800

    fix: use safe-json-stringify replace circular-json

--- src/main/mocha.ts
+++ src/main/mocha.ts
@@ -1,5 +1,4 @@
 import { fork } from 'child_process';
-import * as CircularJSON from 'circular-json';
 import * as debug from 'debug';
 import * as Mocha from 'mocha';
 import { resolve as pathResolve } from 'path';
@@ -148,7 +147,7 @@ export default class MochaWrapper extends Mocha {
     const rootSuite = this.suite;
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const serialized = testArtifacts.syncedSubprocessData!;
-    const { rootSuite: testRootSuite } = CircularJSON.parse(serialized.results, subprocessParseReviver);
+    const { rootSuite: testRootSuite } = JSON.parse(serialized.results, subprocessParseReviver);
 
     Object.assign(testRootSuite, {
       parent: rootSuite,
@@ -161,7 +160,7 @@ export default class MochaWrapper extends Mocha {
   private extractSubprocessRetriedTests(testArtifacts: ISubprocessResult): IRetriedTest[] {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const serialized = testArtifacts.syncedSubprocessData!;
-    const { retriesTests } = CircularJSON.parse(serialized.retries, subprocessParseReviver);
+    const { retriesTests } = JSON.parse(serialized.retries, subprocessParseReviver);
 
     return retriesTests as IRetriedTest[];
   }
